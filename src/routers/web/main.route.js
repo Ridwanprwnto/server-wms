1edc0ca986c413116b7f2fc26adb1bc86cac8203
Purwanto Ridwan <Ridwanprwnto@yahoo.com>
2026-08-08 14:26:24 +0700

    Feat: Sorting pool service implemented in prod

--- src/routers/web/main.route.js
+++ src/routers/web/main.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const moduleMasterATK = require("./modules/master-atk.route.js");
 const modulePlanogramATK = require("./modules/planogram-atk.route.js");
 const moduleStockATK = require("./modules/stock-atk.route.js");
+const moduleMonitoringSortasi = require("./modules/monitoring-sortasi.route.js");
 
 const mainRouter = express.Router();
 
 mainRouter.use("/atk/master", moduleMasterATK);
 mainRouter.use("/atk/planogram", modulePlanogramATK);
 mainRouter.use("/atk/stocks", moduleStockATK);
+mainRouter.use("/sortasi/monitoring", moduleMonitoringSortasi);
 
 module.exports = mainRouter;
